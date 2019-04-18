93b46d65703b7c8e7b0d8d87f84181d61ba0e0a4
GuoLei Sun <sunguolei@100tal.com>
2019-04-18 15:06:24 +0800

    Initial Commit

---
+++ mockGPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mockGPS
+//
+//  Created by Sun GuoLei on 2019/4/18.
+//  Copyright © 2019 Sun GuoLei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
